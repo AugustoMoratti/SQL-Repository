0abfea8f3c0d0bbeadff7940ebe20740dedd38cf
Augusto Moratti <augus.m0804@gmail.com>
2024-11-29 19:42:41 -0300

    Quinta clase ..

---
+++ Writing/01_Insert.sql
@@ -0,0 +1,4 @@
+/*Se encarga de insertar en la tabla*/
+
+INSERT INTO users (name, surname) VALUES ('Roberto', 'Carlo');
+
---
+++ Writing/02_Update.sql
@@ -0,0 +1,9 @@
+/*Actualizar la tabla, siempre se hacen aparte con una regla de filtrado*/
+
+UPDATE users SET age = '21' WHERE user_id = 1;
+
+UPDATE users SET age = 21, name = 'Jose' WHERE user_id = 2;
+
+/* Tener cuidado con los formatos, ver en w3*/
+
+
