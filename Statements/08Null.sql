f07e83aaddbef1c8d20cb383b66b563f305e043e
Augusto Moratti <augus.m0804@gmail.com>
2024-11-29 19:24:45 -0300

    Quinta clase

--- Statements/08Null.sql
+++ Statements/08Null.sql
@@ -2,3 +2,9 @@ SELECT * FROM users WHERE email IS NOT NULL;
 
 SELECT * FROM users WHERE email IS NULL;
 
+/* IFNULL*/
+SELECT name, username, ifnull(age, 0) AS 'Age' FROM users;
+
+/*ISNULL tambien está , veer en w3
+Returna 1 o 0 dependiendo si es nulo o no*/
+
